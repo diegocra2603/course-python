2170c8abe0493fb2640a20607074261d45f324c8
Diego Cruz <diegocra2603@gmail.com>
2023-05-19 07:36:58 -0600

    Se termino el ejercicio de tipos avanzados

--- tipos-avanzados/15-ejercicio.py
+++ tipos-avanzados/15-ejercicio.py
@@ -1,31 +1,46 @@
+
+from pprint import pprint
+
+
 def deleteBlankSpaces(string):
     return [character for character in string if character != ' ']
 
 
 def countCharacters(string):
-    dictonaryChars = {}
+    chars_dict = {}
     for char in string:
-        if char in dictonaryChars:
-            dictonaryChars[char] = dictonaryChars[char] + 1
+        if char in chars_dict:
+            chars_dict[char] += 1
         else:
-            dictonaryChars[char] = 1
-    return dictonaryChars
+            chars_dict[char] = 1
+    return chars_dict
 
-def createTuplaOrder(dictionary):
-    return [  valor for  valor in dictionary.items() ]
+
+def createTuplaOrder(dict):
+    return sorted(dict.items(),
+                  key=lambda key: key[1], reverse=True)
+
 
 def getMaxValuesArrayTupla(tuplas):
-
-    max_value = 0;
+    max_value = 0
     for item in tuplas:
         if item[1] >= max_value:
             max_value = item[1]
-        
-    return [ tupla for tupla in tuplas if tupla[1] >= max_value ]
+
+    return [tupla for tupla in tuplas if tupla[1] >= max_value]
 
 
-stringWithoutBlankSpaces = deleteBlankSpaces("Hola Mundo")
+def show_finish_message(arrayTupla):
+    start_message = f"Los caracteres que mas se repites con {arrayTupla[0][1]} repeticiones son: "
+    for tupla in arrayTupla:
+        start_message += f" \n - {tupla[0].upper()}"
+    return start_message
+
+
+stringWithoutBlankSpaces = deleteBlankSpaces("aabbccdddeee")
 dictionaryWithCountChar = countCharacters(stringWithoutBlankSpaces)
 tuplaOrder = createTuplaOrder(dictionaryWithCountChar)
+tuplasWhitMaxValues = getMaxValuesArrayTupla(tuplaOrder)
 
-print(getMaxValuesArrayTupla(tuplaOrder))
+print(show_finish_message(tuplasWhitMaxValues))
+pprint(tuplaOrder, width=1)
